edaf3574d914c56533103f99a72c99754d58a950
MuhammadAreeb30 <almostevil55@gmail.com>
2024-06-24 20:51:00 +0500

    footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary text-white mt-8 py-2">
+      <div className="max-w-7xl mx-auto text-center">
+        <p className="text-md">
+          Copyright ©2024 All Rights Reserved | This Template is Made with ❤ by
+          Muhammad Areeb.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
